cbefca7d8e83179f7f811f83c259a7724cd5dc71
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-02 23:34:35 +0300

    db context and migration

--- DotNetAPİDemo/Context/ApplicationDbContext.cs
+++ DotNetAPİDemo/Context/ApplicationDbContext.cs
@@ -4,5 +4,10 @@ namespace DotNetAPİDemo.Context
 {
     public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Models.Post> Posts { get; set; }
     }
 }
--- DotNetAPİDemo/Program.cs
+++ DotNetAPİDemo/Program.cs
@@ -1,4 +1,10 @@
+using DotNetAPÝDemo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
