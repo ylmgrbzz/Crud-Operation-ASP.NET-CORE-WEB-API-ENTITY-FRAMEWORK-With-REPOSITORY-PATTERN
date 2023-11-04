728c0e06315ee000710b962757afeb139f8930c8
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-04 15:24:43 +0300

    post controller

--- DotNetAPİDemo/Controllers/PostController.cs
+++ DotNetAPİDemo/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using DotNetAPİDemo.Context;
+using DotNetAPİDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -99,6 +100,75 @@ namespace DotNetAPİDemo.Controllers
                     return CustomResult("Data not found.", HttpStatusCode.NotFound);
                 }
                 return CustomResult("Data loaded successfully.", post);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add(Post post)
+        {
+            try
+            {
+                post.CreatedAt = DateTime.Now;
+                bool isSaved = _postManager.Add(post);
+                if (isSaved)
+                {
+                    return CustomResult("Data saved successfully.", post);
+                }
+                return CustomResult("Data not saved.", HttpStatusCode.BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update(Post post)
+        {
+            try
+            {
+                if (post == null)
+                {
+                    return CustomResult("Data not found.", HttpStatusCode.NotFound);
+                }
+                if (post.Id == 0)
+                {
+                    return CustomResult("Please enter valid id.", HttpStatusCode.BadRequest);
+                }
+
+                bool isUpdated = _postManager.Update(post);
+                if (isUpdated)
+                {
+                    return CustomResult("Data updated successfully.", post);
+                }
+                return CustomResult("Data not updated.", HttpStatusCode.BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var post = _postManager.GetById(id);
+                if (post == null)
+                {
+                    return CustomResult("Data not found", HttpStatusCode.NotFound);
+                }
+                bool isDelete = _postManager.Delete(post);
+                if (isDelete)
+                {
+                    return CustomResult("Post has been deleted.");
+                }
+                return CustomResult("Post deletd failed.", HttpStatusCode.BadRequest);
             }
             catch (Exception ex)
             {
